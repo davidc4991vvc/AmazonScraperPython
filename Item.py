e542345207b1cadc1b30313a8ed2b565031406c4
Mark Porter <m.porter614@gmail.com>
2016-04-30 20:31:07 -0400

    Added basic methods for interacting with items from store

--- Item.py
+++ Item.py
@@ -3,3 +3,28 @@ import pymongo #interfaces with mongo DB to store pricing data
 class Item:
 	name = ""
 	price = 0.00
+	product_code = ""
+	amazon_base_url = "http://www.amazon.com/gp/product/"
+
+	def __init__(self, name, price, product_code):
+		self.name = name
+		self.price = price
+		self.product_code = product_code
+
+	def getName(self):
+		print Item.name
+
+	def getPrice(self):
+		print Item.price
+
+	def getProductCode(self):
+		print Item.product_code
+
+	def setName(self, new_name):
+		self.name = new_name
+	
+	def setPrice(self, new_price):
+		self.price = new_price
+
+	def setProductCode(self, new_product_code):
+		self.product_code = new_product_code
