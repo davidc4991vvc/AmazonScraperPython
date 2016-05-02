0623e994e04c9ce25c45c02f36a5c82be8dfdfd4
Mark Porter <m.porter614@gmail.com>
2016-05-02 02:17:54 -0400

    Filled out MongoDB methods and added logic for interacting with Scraper and MongoDB to main class

--- Main.py
+++ Main.py
@@ -1,35 +1,72 @@
 from Item import Item
+from MongoDBHelper import MongoDBHelper
+from Scraper import Scraper
+import time
 
 print "Amazon Scraper: Where all your frugal dreams come true.\n"
 
-choice = 0
+url = "http://www.amazon.com/gp/product/"
 
-while choice != 4:
-	print "1. Define new item\n"
-	print "2. Check current items\n"
-	print "3. Delete an item\n"
-	print "4. Exit\n"
+database = MongoDBHelper.setupDB()
 
-	choice = raw_input("Please select an option by entering a number, 1 through 4 :")
+cursor = database.items.find()
 
-	try:
-		choice = int(choice)
-	except ValueError:
-		print "Error: input not numeric. Please try again!\n"
-		continue
+if cursor.count() > 0:
+	for document in cursor:
+		print "Updating price data for the day... please wait...\n"
+		temp_product_code = document[product_code]
+		updateScrape = Scraper.amazonScrape(url, temp_product_code)
+		tempItem = Item(updateScrape[name], updateScrape[prices], temp_product_code)
+		MongoDBHelper.updatePriceHistory(tempItem, database)
 
-	if (choice < 0) or (choice > 4):
-		print "Error: choice not defined. Please try again!\n"
+		time.sleep(60)
 
-	elif choice == 1:
-		print "Define new item\n"
-		 #define new item here
-     	elif choice == 2:
-		print "Check item\n"
-		 #check items here
-	elif choice == 3:
-		print "Delete an item\n"
-		 #delete an item here
-	else:
-		print "Exiting now..."
+else:
+
+	choice = 0
+
+	while choice != 4:
+		print "1. Define new item\n"
+		print "2. Check current items\n"
+		print "3. Delete an item\n"
+		print "4. Exit\n"
+
+		choice = raw_input("Please select an option by entering a number, 1 through 4 :")
+
+		try:
+			choice = int(choice)
+		except ValueError:
+			print "Error: input not numeric. Please try again!\n"
+			continue
+
+		if (choice < 0) or (choice > 4):
+			print "Error: choice not defined. Please try again!\n"
+
+		elif choice == 1:
+			print "Define new item\n"
+			 #define new item here
+			prod_code = raw_input("Please enter the Amazon.com product code string for your item: ")
+		
+			newScrape = Scraper.amazonScrape(url, prod_code)
+
+			if len(newScrape) > 0:
+				newItem = Item(newScrape[name], newScrape[prices], prod_code)
+				MongoDBHelper.addItem(newItem, database)
+			else:
+				print "Item not found. Please try again.\n"
+
+	     	elif choice == 2:
+			print "View all items\n"
+			 #view items here
+
+			MongoDBHelper.viewAllItems(database)
+
+		elif choice == 3:
+			print "Delete an item\n"
+			 #delete an item here
+
+			prod_code = raw_input("Please enter the Amazon.com product code string for your item: ")
+			MongoDBHelper.deleteItem(prod_code, database)
+		else:
+			print "Exiting now..."
 
--- MongoDBHelper.py
+++ MongoDBHelper.py
@@ -1,22 +1,76 @@
-from PyMongo import MongoClient
+from pymongo import MongoClient
+from Item import Item
+from datetime import date
 
 class MongoDBHelper:
-
-	def setupDB()
+	
+	@classmethod
+	#Setup DB client and returns relevant database for our work
+	def setupDB(self):
 		client = MongoClient()
 		return client.ItemCollection
 
-	def addItem(item, database)
-		#TODO
-
-	def updatePrice(item, newPrice, database)
-		#TODO -- need to store new price in DB -- append to list
-		item.setPrice(newPrice)
+	@classmethod
+	#Prints all items in DB, or a message if there are none
+	def viewAllItems(self, database):
+		cursor = database.items.find()
 		
-	def recalculateAveragePrice(item, prices, database)
+		if cursor.count() > 0:
+			for document in cursor:
+	    			print(document)
+		else:
+			print "No items in database!\n"
+
+	@classmethod
+	#Adds an item to DB
+	def addItem(self, item, database):
+
+		newEntry = dict(name=item.getName(), product_code=item.getProductCode(), price_history=item.getDatePricePair(), average_price=item.getAveragePrice())
+
+		database.items.insert_one(newEntry)
+		print newEntry + " inserted to database.\n"
+
+	@classmethod
+	#Adds new price contained in Item to its collected prices in DB, and also calls method to update item's average price. Updates DB with these changes.
+	def updatePriceHistory(self, item, database):
+
+		cursor = database.items.find({"product_code": item.getProductCode()})
+
+		for document in cursor:
+			oldPriceHistory = document.pop(price_history)
+			oldPriceHistory[date.today()] = item.getDatePricePair().values().pop()
+
+			document[price_history] = oldPriceHistory
+
+			prices = oldPriceHistory.values()
+			new_average_price = recalculateAveragePrice(prices)
+
+			database.items.update_one({"name": item.getName()},
+ {
+        "$set": {
+            "price_history": oldPriceHistory,
+	    "average_price": new_average_price
+        }
+ }
+)
+
+		print item.getName() + " updated.\n"
+
+	#Helper method to recalculate average price of an item
+	def recalculateAveragePrice(prices):
 		new_average_price = sum(prices) / float(len(prices))
 		item.setAveragePrice(new_average_price)
-		#TODO -- need to store new average in DB
 
-	def deleteItem(item, database)
-		#TODO
+		return new_average_price
+
+	@classmethod
+	#Deletes a item with given product code from DB if it is present
+	def deleteItem(self, product_code, database):
+
+		result = database.items.delete_many	({"product_code": product_code})
+		
+		if result.deleted_count == 0:
+			print "Item not found in database. Please try again.\n"
+		else:
+			print result + " removed from database."
+		
