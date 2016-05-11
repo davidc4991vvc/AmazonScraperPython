f1dd64caacbeea5d84d702073e7c7f23928e9162
Mark Porter <m.porter614@gmail.com>
2016-05-11 10:36:28 -0400

    New version of scraper that uses BeautifulSoup and requests to be able to parse dynamic pages

--- Scraper.py
+++ Scraper.py
@@ -1,20 +1,56 @@
 from datetime import date
-from lxml import html
+from pprint import pprint
+from bs4 import BeautifulSoup
 import requests
 
 class Scraper:
 	
-	def amazonScrape(url, product_code)
+	@classmethod
+	def amazonScrape(self, url, product_code):
 		
-		webpage = requests.get(url+product_code)
-		html_tree = html.fromstring(webpage.content)
+		product_url = url+product_code
+		response = requests.get(product_url, headers={'User-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'})
+
+		soup = BeautifulSoup(response.content)
+		tags = {}
+		for li in soup.select('span#productTitle'):
+		    try:
+			itemTitle = li.getText().strip()
+			#key = title.text.strip().rstrip(':')
+			#value = title.next_sibling.strip()
+
+		    except AttributeError:
+			break
+
+		for li in soup.select('span#priceblock_ourprice'):
+		    try:
+			price = li.getText()
+			#key = title.text.strip().rstrip(':')
+			#value = title.next_sibling.strip()
+
+		    except AttributeError:
+			break
 
 		#Returns list of item titles (1 in our case)	
-		itemTitles = html_tree.xpath('//span[@id="productTitle"]/text()')
+		
+		print "Title: ", itemTitle
 
-		#Returns list of item prices (1 in our case)
-		prices = html_tree.xpath('//span[@id="priceblock_ourprice"]/text()')
+		
+		print "Price: ", price
 
-		#Outputs a dictionary with key value pairs for item name and prices of the item, as well as date of the scrape	
-		price_collection = dict(date.today()=prices.pop())
-		return dict(name=itemTitles.pop(), prices=price_collection)
+		#implicit check for null with python magic
+		if price:
+
+			#Outputs a dictionary with key value pairs for item name and prices of the item, as well as date of the scrape	
+			yyyymmdd = date.today().strftime("%Y%m%d")
+			print "date: ", yyyymmdd
+
+			price_collection = dict()
+			price_collection[yyyymmdd] = price
+
+			return dict(name=itemTitle, price_history=price_collection)
+
+		else:
+			print "Item not able to be scraped. Exiting..."
+			exit(1)
+
